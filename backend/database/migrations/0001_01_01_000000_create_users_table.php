be8eddc6e53c5918a18928499a19201badcff064
jihanejador <jihanejador@gmail.com>
2026-09-08 11:35:57 +0100

    ajouter le role de client

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,11 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['client', 'pro'])->default('client');
             $table->string('phone', 50)->nullable();
             $table->string('photo')->nullable();
             $table->string('address')->nullable();
             $table->string('city')->nullable();
-            $table->enum('role', ['client', 'pro'])->default('client');
             $table->text('description')->nullable();
             $table->string('specialty')->nullable();
             $table->rememberToken();
