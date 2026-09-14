1cd2623fb58ec3dac8680b6ea6407e786fbf7a0f
jihanejador <jihanejador@gmail.com>
2026-09-14 12:35:22 +0100

    fix(services): consolidate axios instance and endpoints into serviceApi.js

--- frontend/src/services/serviceApi.js
+++ frontend/src/services/serviceApi.js
@@ -1,9 +1,20 @@
-import API from './axios'; 
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 
 export const getServices = (params = {}) => API.get('/services', { params });
-
 export const getProProfile = (id) => API.get(`/pros/${id}`);
-
 export const createService = (data) => API.post('/services', data);
 export const updateService = (id, data) => API.put(`/services/${id}`, data);
 export const deleteService = (id) => API.delete(`/services/${id}`);
