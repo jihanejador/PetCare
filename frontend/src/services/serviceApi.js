6d2034dbd6b9d34352a902b0d80f358d63dacca9
jihanejador <jihanejador@gmail.com>
2026-09-14 12:23:40 +0100

    feat(api): update serviceApi with filter params support and getProProfile endpoint

--- frontend/src/services/serviceApi.js
+++ frontend/src/services/serviceApi.js
@@ -1,18 +1,9 @@
-import axios from 'axios';
+import API from './axios'; 
 
-const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', 
-});
+export const getServices = (params = {}) => API.get('/services', { params });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+export const getProProfile = (id) => API.get(`/pros/${id}`);
 
-export const getServices = () => API.get('/services');
 export const createService = (data) => API.post('/services', data);
 export const updateService = (id, data) => API.put(`/services/${id}`, data);
 export const deleteService = (id) => API.delete(`/services/${id}`);
