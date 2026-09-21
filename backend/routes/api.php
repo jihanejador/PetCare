efaca04cc01378b05640043a9771a6235d5250b9
jihanejador <jihanejador@gmail.com>
2026-09-21 12:01:51 +0000

    add route of mark message as read

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -51,4 +51,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/messages/conversations', [MessageController::class, 'getConversations']);
     Route::get('/messages/{otherUserId}', [MessageController::class, 'getConversation']);
     Route::post('/messages', [MessageController::class, 'sendMessage']);
+    Route::post('/messages/read/{senderId}', [MessageController::class, 'markAsRead']);
 });
